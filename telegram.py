74dd80cc6b69f378bd0659cf8243282e296cc060
zheev <kz.zhebrikov@yandex.ru>
2019-04-22 09:00:14 +0300

    add import vars

--- telegram.py
+++ telegram.py
@@ -1,5 +1,5 @@
 import requests
-
+import vars
 
 def getUrl(TOKEN, type):
     return "https://api.telegram.org/bot{0}/{1}".format(TOKEN, type)
@@ -15,12 +15,12 @@ def getPhoto(photo):
 
 def sendPhoto(text, photo):
     photoPath = getPhoto(photo)
-    url = getUrl(TOKEN, 'sendPhoto')
+    url = getUrl(vars.TOKEN, 'sendPhoto')
     files = {'photo': open(photoPath, 'rb')}
     r = requests.post(url, data={'chat_id': '@zheevkino', 'caption': text}, files=files)
     print(r.json())
 
 
 def sendMessage(text):
-    url = getUrl(TOKEN, 'sendMessage')
+    url = getUrl(vars.TOKEN, 'sendMessage')
     requests.post(url, data={'chat_id':'@zheevkino', 'text':text, 'parse_mode':'Markdown'})
