601d7de71fc16d36a9fe408c00bf40e48a9baa0f
zheev <kz.zhebrikov@yandex.ru>
2019-04-29 10:10:32 +0300

    delete print from sendPhoto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import db_wall
 import telegram
+from vars import ACCESS_TOKEN
 
 from urllib.parse import urlencode
 domain = 'https://api.vk.com/method/wall.get?'
@@ -11,7 +12,7 @@ def getWall(**kwargs):
 
 if __name__ == '__main__':
     list = getWall(domain='kino_mania', count=5, offset=1,
-                    access_token='017d1e79017d1e79017d1e79f301173f3f0017d017d1e795dc1a1d713d84d403724c3a5',
+                    access_token= ACCESS_TOKEN,
                     v='5.95')['response']['items']
 
     maxId = db_wall.getLastIdWall()
@@ -21,7 +22,7 @@ if __name__ == '__main__':
             photos  = wall['attachments'][0]['photo']['sizes']
             photoSend = photos[len(photos)-1]
             if maxId:
-                if maxId[0] < wall['id']:
+                if int(maxId[0]) < int(wall['id']):
                     telegram.sendPhoto(wall['text'], photoSend['url'])
             else:
                 telegram.sendPhoto(wall['text'], photoSend['url'])
