7741dd42b939a7bad72c14678602d1e5ad879eb3
zheev <kz.zhebrikov@yandex.ru>
2019-05-03 10:41:38 +0300

    refactoring

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ from vars import ACCESS_TOKEN
 from urllib.parse import urlencode
 domain = 'https://api.vk.com/method/wall.get?'
 
-def getWall(**kwargs):
+def getWalls(**kwargs):
     r = requests.get(domain+urlencode(kwargs))
     return r.json()
 
 if __name__ == '__main__':
-    list = getWall(domain='kino_mania', count=5, offset=1,
+    walls = getWalls(domain='kino_mania', count=5, offset=1,
                     access_token= ACCESS_TOKEN,
                     v='5.95')['response']['items']
 
     maxId = db_wall.getLastIdWall()
 
-    for wall in list:
+    for wall in walls:
         if wall['attachments']:
             photos  = wall['attachments'][0]['photo']['sizes']
-            photoSend = photos[len(photos)-1]
+            photo_for_send = photos[len(photos)-1]
             if maxId:
                 if int(maxId[0]) < int(wall['id']):
-                    telegram.sendPhoto(wall['text'], photoSend['url'])
+                    telegram.sendPhoto(wall['text'], photo_for_send['url'])
             else:
-                telegram.sendPhoto(wall['text'], photoSend['url'])
+                telegram.sendPhoto(wall['text'], photo_for_send['url'])
 
     db_wall.insertIdWall(list[0]['id'])
